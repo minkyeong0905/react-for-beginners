b0c4ff15c9145871c7e2fb97aa77c8ccc7b0dafe
minkyeong0905 <mingit0905@naver.com>
2025-06-25 16:16:39 +0900

    Movie app

--- src/App.js
+++ src/App.js
@@ -2,52 +2,29 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [amount, setAmount] = useState(0);
-  const [id, setId] = useState("");
-
-  const selectedCoin = coins.find((item) => item.id === id);
-  
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then(response => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-        setId(json[0]?.id || "")
-      });
-  }, []);
-
-  const onCoinSelect = (event) => {
-    setId(event.target.value);
-  };
-
-  const onChange = (event) => {
-    setAmount(event.target.value);
-  };
-
-  return <div>
-    <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-    {loading ? <strong>Loading...</strong> : (
-    <div>
-      <select onChange={onCoinSelect} value={id}>
-        {coins.map((coin) => <option key={coin.id} value={coin.id}>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD</option>)}
-      </select>
-
-      <hr />
-
-      <div>
-        <label htmlFor="usd">USD($) </label>
-        <input id="usd" type="number" placeholder="USD" value={amount} onChange={onChange} />
-      </div>
-
-      <div>
-        <label htmlFor="coin">{selectedCoin.name} ({selectedCoin.symbol}) </label>
-        <input id="coin" type="number" disabled={true} value={selectedCoin.quotes.USD.price > 0 ? (amount / selectedCoin.quotes.USD.price).toFixed(5) : 0} />
-      </div>
-    </div>
-    )}
+  const [movies, setMovies] = useState([]);
+  const getMovies = async() => {
+    // const response = await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year");
+    // const json = await response.json();
+    const json = await (await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=10&sort_by=year")).json();
     
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+
+  useEffect(() => {getMovies();}, []);
+  
+  return <div>
+    {loading ? <h1>Loading...</h1> : <div>{movies.map((movie) => (
+      <div key={movie.id}>
+        <img src={movie.medium_cover_image} />
+        <h2>{movie.title}</h2>
+        <ul>
+          {movie.genres.map((item, index) => <li key={index}>{item}</li>)}
+        </ul>
+        <p>{movie.summary}</p>
+      </div>
+    ))}</div>}
   </div>;
 }
 
