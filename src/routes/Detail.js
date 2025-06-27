f8a7936bb386ea5e1af54b2f2fd3ad9744675893
minkyeong0905 <mingit0905@naver.com>
2025-06-27 15:08:50 +0900

    Updated Detail.js

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,18 +1,43 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function Detail() {
+    const [loading, setLoading] = useState(true);
+    const [detail, setDetail] = useState(null);
+    const [genres, setGenres] = useState([]);
     const { id }= useParams();
+
     const getMovie = async () => {
         const json = await (await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)).json();
-        console.log(json);
+        
+        setDetail(json.data.movie);
+        setGenres(json.data.movie.genres);
+        setLoading(false);
     };
 
-    useEffect(() => {
-        getMovie();
-    }, []);
+    useEffect(() => {getMovie();}, []);
 
-    return <h1>Detail</h1>
+    return <div>
+        {
+            loading ? <h1>Loading...</h1> :
+            <div>
+                <h1>{detail.title_long}</h1>
+                <img src={detail.medium_cover_image} alt={detail.title} />
+                <h3>{`Language : ${detail.language}`}</h3>
+                <h3>{`Rating : ${detail.rating}`}</h3>
+                <h3>{`Runtime : ${detail.runtime} min`}</h3>
+                <h3>Genres</h3>
+                <ul>
+                    {
+                        genres.map((item, index) => (
+                            <li key={index}>{item}</li>
+                        ))
+                    }
+                </ul>
+                <h3>{`Likes : ${detail.like_count}`}</h3>
+            </div>
+        }
+    </div>;
 }
 
 export default Detail;
